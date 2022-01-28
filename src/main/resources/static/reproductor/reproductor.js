a2d87fff40be6e72f3cf22371192c487474798c1
FacuVerge <facu.verge@gmail.com>
2022-01-28 16:48:56 -0300

    Modifico estilo de botones y agrego pistas de audio

--- src/main/resources/static/reproductor/reproductor.js
+++ src/main/resources/static/reproductor/reproductor.js
@@ -10,7 +10,13 @@ var app1 = new Vue({
     		nombre: null,
     		urlPista: null,
     		artistas: [],
-    		album: null,
+    		album: {
+    			id: null,
+    			nombre: null,
+    			anio: null,
+    			urlPortada: null,
+    			artistas: []
+    		},
     		generos: [],
     		artistasMapeados: null
     	}],
@@ -26,22 +32,31 @@ var app1 = new Vue({
     },
     methods:{
     	anteriorCancion() {
+    		this.frenarMusica()
         	if(this.indice == 0) {
         		this.indice = this.canciones.length - 1
         	}else{
         		this.indice = this.indice - 1
         	}
+        	this.reproducirMusica()
         },
         siguienteCancion() {
+        	this.frenarMusica()
         	if(this.indice >= this.canciones.length - 1) {
         		this.indice = 0
             }else{
        			this.indice = this.indice + 1
        		}
+       		this.reproducirMusica()
+        },
+        frenarMusica() {
+        	document.getElementById(this.canciones[this.indice].id).load();
         },
         pausarMusica() {
+			document.getElementById(this.canciones[this.indice].id).pause();
         },
         reproducirMusica() {
+       		document.getElementById(this.canciones[this.indice].id).play();
         }
     }
 })
