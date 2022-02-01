4a4845d19d377979ce5ebfb55dcb77df61f28547
FacuVerge <facu.verge@gmail.com>
2022-02-01 15:29:01 -0300

    Intento reducir heap use

--- src/main/java/spotifach/Security/SecurityConfiguration.java
+++ src/main/java/spotifach/Security/SecurityConfiguration.java
@@ -1,11 +1,8 @@
 package spotifach.Security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -13,11 +10,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.authorizeRequests().antMatchers("/**").permitAll();
 		httpSecurity.cors().and().csrf().disable();
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 
 }
